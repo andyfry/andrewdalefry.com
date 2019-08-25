9db1c095b296eb4f42c2c42c5290cea50099c856
Andy Fry <andrewfalefry@gmail.com>
2019-08-25 16:49:54 -0500

    Converted from liquid template to nunjucks.

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     return {
       templateFormats: [
         "md",
+        "jpg",
         "css" // css is not yet a recognized template extension in Eleventy
       ]
     };
