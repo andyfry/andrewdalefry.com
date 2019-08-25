6f91562654c47c4729713298c3235d0fde2ecc54
Andy Fry <andrewfalefry@gmail.com>
2019-08-25 16:58:45 -0500

    Added a date formating filter.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,16 @@
-module.exports = function(eleventyConfig) {
-    return {
-      templateFormats: [
-        "md",
-        "jpg",
-        "css" // css is not yet a recognized template extension in Eleventy
-      ]
-    };
+const moment = require('moment');
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addFilter('date', date => {
+    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  });
+
+  return {
+    templateFormats: [
+      "md",
+      "jpg",
+      "css" // css is not yet a recognized template extension in Eleventy
+    ]
   };
+};
