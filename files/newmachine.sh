047d26c2a17fb24e5c4b990e3b12b4f0d42a5dbd
Andy Fry <andrewfalefry@gmail.com>
2020-08-05 01:16:26 -0500

    "updating new machine script to sudo"

--- files/newmachine.sh
+++ files/newmachine.sh
@@ -19,6 +19,16 @@ fail () {
 echo ''
 echo "Setting Up Machine"
 
+
+info "Prompting for sudo password..."
+if sudo -v; then
+    # Keep-alive: update existing `sudo` time stamp until `setup.sh` has finished
+    while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+    success "Sudo credentials updated."
+else
+    error "Failed to obtain sudo credentials."
+fi
+
 info "Installing XCode command line tools..."
 if xcode-select --print-path &>/dev/null; then
     success "XCode command line tools already installed."
